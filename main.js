34a4b0d9f757f0a2dbbd1117795202ac24109960
Jun <47633500+JunkieUnicorn@users.noreply.github.com>
2021-01-12 16:19:42 +0100

    affichage fonctionnel

--- main.js
+++ main.js
@@ -7,16 +7,28 @@ const port = 9090;
 var array_dossier = fs.readdirSync("./");
 
 app.get('/', (req, res)=>{
-    test_fs.affichage_contenu_dossier(array_dossier, res);
+    //test_fs.affichage_contenu_dossier(array_dossier, res);
     array_dossier.forEach(file =>{
         if (fs.lstatSync(`./${file}/`).isDirectory()){
-            res.write(`<div id=${file}>${file}`)
-            console.log(fs.readdirSync(`./${file}/`));
-        }else
             console.log(file);
-        res.write(`<div href="./${file}">${file}</div>`, encoding='utf-8');
+            //ouverture de la div principale 
+            res.write(`<div id=${file}>${file}`);
+            //création du tableau secondaire contenant les sous-répertoires
+            array_tmp = fs.readdirSync(`./${file}/`);
+            //ouverture de la div secondaire
+            res.write(`<div style="margin-left:5%">`);
+            array_tmp.forEach(file =>{
+                console.log('\t'+file);
+                res.write(`<div style="color:green" id=${file}>${file}</div>`);    
+            })
+            //fermeture de la div principale
+            res.write(`</div>`);
+            res.write(`</div>`);
+        }else{
+            console.log(file);
+            res.write(`<div id="${file}">${file}</div>`, encoding='utf-8');
+        }
     })
-    //res.write(`<div href="./${file}">${file}</div>`, encoding='utf-8');
 });
 
 app.listen(port, () =>{
