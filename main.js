573aa5085ea61fcb3c4792aa1c64faea53bdd711
Jun <47633500+JunkieUnicorn@users.noreply.github.com>
2021-01-12 17:10:34 +0100

    séparation des fichiers fonctionnels :ok_hand:

--- main.js
+++ main.js
@@ -6,29 +6,39 @@ const port = 9090;
 
 var array_dossier = fs.readdirSync("./");
 
+function reveal(id){
+    element = document.getElementById(id);
+
+    if (window.getComputedStyle(element).visibility == "collapse")
+        element.style.visibility = "visible";
+    else
+        element.style.visibility = "collapse";
+}
+
 app.get('/', (req, res)=>{
-    //test_fs.affichage_contenu_dossier(array_dossier, res);
-    array_dossier.forEach(file =>{
+    test_fs.affichage_contenu_dossier(array_dossier, res);
+    /*array_dossier.forEach(file =>{
         if (fs.lstatSync(`./${file}/`).isDirectory()){
             console.log(file);
             //ouverture de la div principale 
-            res.write(`<div id=${file}>${file}`);
+            res.write(`<div id=${file} onclick="reveal('${file}_content');">${file}`);
             //création du tableau secondaire contenant les sous-répertoires
             array_tmp = fs.readdirSync(`./${file}/`);
             //ouverture de la div secondaire
-            res.write(`<div style="margin-left:5%">`);
+            res.write(`<div style="margin-left: 5%;" id="${file}_content">`);
             array_tmp.forEach(file =>{
                 console.log('\t'+file);
                 res.write(`<div style="color:green" id=${file}>${file}</div>`);    
             })
-            //fermeture de la div principale
+            //fermeture de la div secondaire
             res.write(`</div>`);
+            //fermeture de la div principale
             res.write(`</div>`);
         }else{
             console.log(file);
             res.write(`<div id="${file}">${file}</div>`, encoding='utf-8');
         }
-    })
+    })*/
 });
 
 app.listen(port, () =>{
--- test_fs.js
+++ test_fs.js
@@ -8,13 +8,25 @@ module.exports = {
 
 function affichage_dossier(array_dossier, res){
     array_dossier.forEach(file =>{
-        try{
-            if (fs.lstatSync(`./${file}/`).isDirectory())
-            affichage_dossier(fs.readdirSync(`./${file}/`));
-        else
-            res.write(file);
-        }catch(error){
-
+        if (fs.lstatSync(`./${file}/`).isDirectory()){
+            console.log(file);
+            //ouverture de la div principale 
+            res.write(`<div id=${file} onclick="reveal('${file}_content');">${file}`);
+            //création du tableau secondaire contenant les sous-répertoires
+            array_tmp = fs.readdirSync(`./${file}/`);
+            //ouverture de la div secondaire
+            res.write(`<div style="margin-left: 5%;" id="${file}_content">`);
+            array_tmp.forEach(file =>{
+                console.log('\t'+file);
+                res.write(`<div style="color:green" id=${file}>${file}</div>`);    
+            })
+            //fermeture de la div secondaire
+            res.write(`</div>`);
+            //fermeture de la div principale
+            res.write(`</div>`);
+        }else{
+            console.log(file);
+            res.write(`<div id="${file}">${file}</div>`, encoding='utf-8');
         }
     })
 }
