f593edf69f33054567b88af3321abac57bd86b27
Alexandra <21810015@etu.unicaen.fr>
2021-01-12 14:04:39 +0100

    a

--- test_fs.js
+++ test_fs.js
@@ -2,12 +2,7 @@ const fs = require('fs');
 
 module.exports = {
     affichage_contenu_dossier : function(array_dossier){
-        array_dossier.forEach(file =>{
-            if (fs.lstatSync(`./${file}/`).isDirectory())
-                affichage_dossier(fs.readdirSync(`./${file}/`));
-            else
-                console.log(file);
-        })
+        affichage_dossier(array_dossier);
     }
 }
 
