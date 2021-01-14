55df2b5704dc17df144b88d453b66b303b1089c6
Jun <47633500+JunkieUnicorn@users.noreply.github.com>
2021-01-14 16:16:21 +0100

    EJS fonctionnel, script js également fonctionnel

--- main.js
+++ main.js
@@ -3,11 +3,16 @@ const fs = require('fs');
 const test_fs = require('./test_fs');
 const app = express();
 const port = 9090;
+var array = new Array();
 
 var array_dossier = fs.readdirSync("./");
 app.set('view engine', 'ejs');
 app.get('/', (req, res)=>{
-    res.render('./main.ejs');
+    array.push(test_fs.affichage_contenu_dossier(array_dossier, res, "./"));
+    //console.log(array);
+    res.render('./main.ejs', {
+        div:array
+    });
     //test_fs.affichage_contenu_dossier(array_dossier, res, "./");
 });
 
--- test_fs.js
+++ test_fs.js
@@ -1,23 +1,29 @@
 const fs = require('fs');
 var path = './';
+var return_array = new Array();
 
 module.exports = {
     affichage_contenu_dossier : function(array_dossier, res, path){
-        affichage_dossier(array_dossier, res, path);
+        return affichage_dossier(array_dossier, res, path);
     }
 }
 
 function affichage_dossier(array_dossier, res, path){
     array_dossier.forEach(file =>{
         var path_tmp = path+file+'/'
-        console.log(file)
+        //console.log(file)
         if (fs.lstatSync(path_tmp).isDirectory()){
             //ouverture de la div principale 
-            res.write(`<div id=${file} onclick="reveal('${file}_rep');">${file}`);
+                //res.write(`<div id=${file} onclick="reveal('${file}_rep');">${file}`);
+            return_array.push(`
+            <img src="../test/images/folder.png" class="card-img-top" alt="logo dossier"> 
+            <div class="card-body">
+            <div id=${file} class="ml-5, card-text" onclick="reveal('${file}_rep');">${file}`)
             //création du tableau secondaire contenant les sous-répertoires
             array_tmp = fs.readdirSync(path_tmp);
             //ouverture de la div secondaire
-            res.write(`<div style="margin-left: 5%;" id="${file}_rep">`);
+                //res.write(`<div style="margin-left: 5%;" id="${file}_rep">`);
+            return_array.push(`<div style="margin-left: 5%; visibility: collapse, display:none" id="${file}_rep">`)
             array_tmp.forEach(file =>{
                 try{
                     //appel récursif de la fonction pour afficher les sous-dossiers
@@ -27,13 +33,16 @@ function affichage_dossier(array_dossier, res, path){
                 }
             })
             //fermeture de la div secondaire
-            res.write(`</div>`);
+                return_array.push(`</div>`);
             //fermeture de la div principale
-            res.write(`</div>`);
+                return_array.push(`</div></div>`);
         }else{
             //affichage dans le cas où il s'agit d'un fichier simple
-            console.log(file);
-            res.write(`<div id="${file}_content">${file}</div>`, encoding='utf-8');
+            //console.log(file);
+                //res.write(`<div id="${file}_content">${file}</div>`, encoding='utf-8');
+            return_array.push(`<div id="${file}_content" class="ml-5">${file}</div>`)
         }
     })
+    //console.log(return_array);
+    return return_array;
 }
