eb4059a514d22546c7f233793bba4a4c30d262d1
Jun <47633500+JunkieUnicorn@users.noreply.github.com>
2021-01-14 17:20:38 +0100

    bug visibility fix, les éléments collapsent correctement

--- test_fs.js
+++ test_fs.js
@@ -13,16 +13,15 @@ function affichage_dossier(array_dossier, path){
         var path_tmp = path+file+'/'
         if (fs.lstatSync(path_tmp).isDirectory()){
             //ouverture de la div principale 
-                //res.write(`<div id=${file} onclick="reveal('${file}_rep');">${file}`);
             return_array.push(`
-            <img src="../test/images/folder.png" class="card-img-top" alt="logo dossier"> 
+            <div class="card, ml-5">
+            <img src="/test/images/folder.png" class="card-img-top" alt="logo dossier"> 
             <div class="card-body">
-            <div id=${file} class="ml-5, card-text" onclick="reveal('${file}_rep');">${file}`)
+            <div id=${file} class="card-text" onclick="reveal('${file}_rep');">${file}`)
             //création du tableau secondaire contenant les sous-répertoires
             array_tmp = fs.readdirSync(path_tmp);
             //ouverture de la div secondaire
-                //res.write(`<div style="margin-left: 5%;" id="${file}_rep">`);
-            return_array.push(`<div style="margin-left: 5%; visibility: collapse" id="${file}_rep">`)
+            return_array.push(`<div style="display:none" id="${file}_rep">`)
             array_tmp.forEach(file =>{
                 try{
                     //appel récursif de la fonction pour afficher les sous-dossiers
@@ -34,14 +33,17 @@ function affichage_dossier(array_dossier, path){
             //fermeture de la div secondaire
                 return_array.push(`</div>`);
             //fermeture de la div principale
-                return_array.push(`</div></div>`);
+                return_array.push(`</div></div></div>`);
         }else{
             //affichage dans le cas où il s'agit d'un fichier simple
-            //console.log(file);
-                //res.write(`<div id="${file}_content">${file}</div>`, encoding='utf-8');
-            return_array.push(`<div id="${file}_content" class="ml-5">${file}</div>`)
+            return_array.push(`
+            <div class="card, ml-5"
+            <img src="/test/images/document.png" class="card-img-top" alt="logo fichier">
+            <div class="card-body">
+            <div id="${file}_content">${file}</div>
+            </div>
+            </div>`);
         }
     })
-    //console.log(return_array);
     return return_array;
 }
